29ed797a6c5a5db8a184d9dcf2ad83f351e198a9
shanya1705 <gautami1705@gmail.com>
2024-03-31 00:19:35 +0530

    hashing oa question solved

---
+++ FindNumberOfTimesAWordCanBeFormedFromAString.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindNumberOfTimesAWordCanBeFormedFromAString {
+    public static void main(String[] args) {
+        String s="mononom";
+        String t="mon";
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i),map.get(s.charAt(i))+1);
+            }
+            else map.put(s.charAt(i),1);
+        }
+        int count=Integer.MAX_VALUE;
+        for(int i=0;i<t.length();i++){
+            if(!map.containsKey(t.charAt(i))){
+                System.out.println(0);
+                System.exit(0);
+            }else {
+                int curr=map.get(t.charAt(i));
+                count= Math.min(curr,count);
+            }
+        }
+        System.out.println(count);
+
+    }
+}
