7bc109031e5402899da061b439033b2f16733b47
shanya1705 <gautami1705@gmail.com>
2024-03-31 22:56:56 +0530

    hashing oa question solved

--- HASHING/RangeQueryOA.java
+++ HASHING/RangeQueryOA.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class RangeQueryOA {
     public static void main(String[] args) {
+
+       /* Ques Desc-  Given an array of size N; in 1 operation you can change any a[i] to A[i]+x; you can do these operations only 1 time on each index. Also x should lie in [-k,k]; after doing all these operations;pick up the largest possible set of equal numbers.
+
+                N=3
+                [5,8,10] and k=2
+->[5(5+0),8(8+0),8(10-2)]---------> so the answer is 2
+        Constraints-
+                1<=n<=100000
+        1<=k<=100000
+        100000<=a[i]>=k+1*/
         Scanner scanner = new Scanner(System.in);
 
         long n = scanner.nextLong();
