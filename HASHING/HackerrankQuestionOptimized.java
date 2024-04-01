68e23d9a956cc5aac80c7747ff8d221401e9dd83
shanya1705 <gautami1705@gmail.com>
2024-04-01 12:40:36 +0530

    hashing oa question solved

--- HASHING/HackerrankQuestionOptimized.java
+++ HASHING/HackerrankQuestionOptimized.java
@@ -6,10 +6,8 @@ public class HackerrankQuestionOptimized {
 
     //optimized solution using range query trick to add a[l]+1 and a[r+1]-1
 
-    public static int convert(String s) {
-        // This method converts time string to integer
-        // 00:12 ----> converted to ---> 12.
-        // 01:01-----> converted to ---> 61.
+    public
+    static int convert(String s) {
         int u1 = s.charAt(0) - '0';
         int u5 = s.charAt(1) - '0';
 
@@ -23,7 +21,7 @@ public class HackerrankQuestionOptimized {
         return v;
     }
 
-    public static void convertIntegerToTimeString(int g) {
+    static void ck(int g) {
         int u = g / 60;
         int y = g % 60;
 
@@ -45,50 +43,50 @@ public class HackerrankQuestionOptimized {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int n = scanner.nextInt();
         int k = scanner.nextInt();
 
-        // Initialize array y with zeros
         int[] y = new int[1441];
 
-        String start, end, a, b;
-        // assuming these inputs are given
-
-        // O(N^2) loop
         for (int i = 0; i < n; i++) {
-            a = scanner.next();
-            b = scanner.next();
-            start = scanner.next();
-            end = scanner.next();
+            String a = scanner.next();
+            String b = scanner.next();
+            String v1 = scanner.next();
+            String v5 = scanner.next();
 
-            int t1 = convert(start);
-            int t5 = convert(end);
+            int t1 = convert(v1);
+            int t5 = convert(v5);
 
             y[t1]++;
             y[t5 + 1]--;
         }
 
+        int i = 1;
+        while (i < 1440) {
+            y[i] = y[i] + y[i - 1];
+            i++;
+        }
+
         int c = 0;
         int g = 0;
-        for (int i = 0; i < 1440; i++) {
 
-            if (y[i] == 0) {
+        for (int j = 0; j < 1440; j++) {
+            if (y[j] == 0) {
                 c++;
 
                 if (c == k) {
-                    convertIntegerToTimeString(i - k + 1);
+                    ck(j - k + 1); // convert this to (hh:mm)
                     g = 1;
                     break;
                 }
-
             } else {
                 c = 0;
             }
-
         }
 
         if (g == 0) {
-            System.out.print("-1");
+            System.out.println("-1");
         }
     }
 }
