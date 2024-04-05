98e332d1fc9df9ef0423bb7ffba1c03e73b2c334
shanya1705 <gautami1705@gmail.com>
2024-04-05 15:42:57 +0530

    Longest subarray having sum of elements atmost K

--- TwoPointers/LongestSubarrayInSortedArrayWithMaxDiffK.java
+++ TwoPointers/LongestSubarrayInSortedArrayWithMaxDiffK.java
@@ -2,13 +2,14 @@ package TwoPointers;
 
 import java.util.Scanner;
 
-class LongestSubarraySumAtMostK {
+class LongestSubarrayInSortedArrayWithMaxDiffK {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int k = scanner.nextInt();
         int[] b = new int[n];
+
         for (int i = 0; i < n; i++) {
             b[i] = scanner.nextInt();
         }
@@ -16,48 +17,47 @@ class LongestSubarraySumAtMostK {
         int i = 0; // Change to 0-based index
         int j = 0; // Change to 0-based index
         int maxLen = 0;
+        int diff = 0;
 
-        int sum = b[0]; // Change to 0-based index
         while (i < n && j < n) {
+            diff = b[j] - b[i];
+
             if (i == j) {
-                if (b[i] > k) {
+                if (diff > k) {
                     i++;
                     j++;
-                    if(i < n){
-                        sum = b[i];
+
+                    if (i < n) {
+                        diff = b[j] - b[i];
                     }
                 } else {
-                    int curLen = 1;
-                    maxLen = Math.max(curLen, maxLen);
-
-                    j++;
-                    if(j < n){
-                        sum += b[j];
-                    }
-                }
-            } else {
-                if (sum > k) {
-                    //[i....j] is invalid but [i......j-1] was valid
-                    //so->[i+1...j-1] will also be valid hence
-                    //i++ and j--
-
-                    sum -= b[i];
-                    i++;
-                    sum -= b[j];
-                    j--;
-                    if (i > j) {
-                        j = i;
-                    }
-                } else {
-                    int curLen = Math.abs(i - j) + 1;
-
+                    int currLen = 1;
+                    maxLen = Math.max(currLen, maxLen);
                     j++;
 
                     if (j < n) {
-                        sum += b[j];
+                        diff = b[j] - b[i];
+                    }
+                }
+            } else {
+                if (diff > k) {
+                    i++;
+                    j--;
+
+                    if (i > j) {
+                        j = i;
                     }
 
-                    maxLen = Math.max(curLen, maxLen);
+                    diff = b[j] - b[i];
+                } else {
+                    int currLen = Math.abs(i - j) + 1;
+                    j++;
+
+                    if (j < n) {
+                        diff = b[j] - b[i];
+                    }
+
+                    maxLen = Math.max(currLen, maxLen);
                 }
             }
         }
